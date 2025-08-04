78c6a9ea18f55184a1bfef0a521af63e4e2f7ec1
Muralee2 <muralimvkaws1@gmail.com>
2025-08-04 20:51:51 +0530

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -2,11 +2,3 @@ resource "google_compute_network" "vpc" {
   name                    = "custom-vpc"
   auto_create_subnetworks = false
 }
-
-resource "google_compute_subnetwork" "subnet" {
-  name          = "private-subnet"
-  ip_cidr_range = "10.0.0.0/24"
-  region        = var.region
-  network       = google_compute_network.vpc.id
-  private_ip_google_access = true
-}
