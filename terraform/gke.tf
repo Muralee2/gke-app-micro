87015dec1fd48acd84a01c3d91c977cd6d6b7490
Muralee2 <muralimvkaws1@gmail.com>
2025-08-04 23:23:57 +0530

    Update gke.tf

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -1,43 +1,34 @@
 resource "google_container_cluster" "primary" {
   name     = var.cluster_name
   location = var.region
-  
+
   remove_default_node_pool = true
   initial_node_count       = 1
 
-  network    = google_compute_network.vpc.name
-  subnetwork = google_compute_subnetwork.subnet.name
-
-  private_cluster_config {
-    enable_private_nodes    = true
-    enable_private_endpoint = true
-    master_ipv4_cidr_block  = "172.16.0.0/28"
-  }
+  network    = google_compute_network.vpc.self_link
+  subnetwork = google_compute_subnetwork.subnet.self_link
 
   ip_allocation_policy {
     cluster_secondary_range_name  = "pods"
     services_secondary_range_name = "services"
   }
 
-  master_auth {
-    client_certificate_config {
-      issue_client_certificate = false
-    }
+  node_config {
+    service_account = "gke-node-sa@${var.project_id}.iam.gserviceaccount.com"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
   }
-
-  depends_on = [
-    google_project_service.services
-  ]
 }
 
 resource "google_container_node_pool" "primary_nodes" {
-  name       = "default-node-pool"
+  name       = "primary-node-pool"
   location   = var.region
   cluster    = google_container_cluster.primary.name
   node_count = 1
 
   node_config {
-    machine_type = "e2-micro"
+    service_account = "gke-node-sa@${var.project_id}.iam.gserviceaccount.com"
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
