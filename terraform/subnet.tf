82b099b52adbb8fabd22d6f46db3078bfa024a6b
Muralee2 <muralimvkaws1@gmail.com>
2025-08-04 20:46:44 +0530

    Update subnet.tf

--- terraform/subnet.tf
+++ terraform/subnet.tf
@@ -1,8 +1,8 @@
 resource "google_compute_subnetwork" "subnet" {
-  name          = "private-subnet"
-  ip_cidr_range = "10.0.0.0/16"
-  region        = var.region
-  network       = google_compute_network.vpc.id
+  name                     = "private-subnet"
+  ip_cidr_range            = "10.0.0.0/16"
+  region                   = var.region
+  network                  = google_compute_network.vpc.id
   private_ip_google_access = true
 
   secondary_ip_range {
