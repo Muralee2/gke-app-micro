026125bf233bdcb0cacee29d9b1c1eabcc77e720
Muralee2 <muralimvkaws1@gmail.com>
2025-08-04 19:02:03 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,22 +1,9 @@
-variable "project_id" {
-  description = "GCP Project ID"
-  type        = string
-}
-
-variable "region" {
-  description = "GCP Region"
-  type        = string
-  default     = "us-central1"
-}
-
-variable "cluster_name" {
+output "cluster_name" {
   description = "GKE Cluster Name"
-  type        = string
-  default     = "secure-microservice"
+  value       = var.cluster_name
 }
 
-variable "dns_name" {
-  description = "DNS name for the service"
-  type        = string
-  default     = "microservice.example.com"
+output "region" {
+  description = "GCP Region"
+  value       = var.region
 }
