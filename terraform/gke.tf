ac0f55f4e5597e5dad7b1b2ffd01d8f1cfe2f49f
Muralee2 <muralimvkaws1@gmail.com>
2025-08-05 14:42:08 +0530

    Update gke.tf

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -1,40 +1,21 @@
-resource "google_container_cluster" "primary" {
-  name     = var.cluster_name
-  location = var.region
+data "google_service_account" "gke_workload_sa" {
+  account_id = "gke-workload-sa"
+  project    = var.project_id
+}
 
+resource "google_container_cluster" "primary" {
+  name                     = "my-gke-cluster"
+  location                 = var.region
   remove_default_node_pool = true
   initial_node_count       = 1
 
-  network    = google_compute_network.vpc.self_link
-  subnetwork = google_compute_subnetwork.subnet.self_link
-
-  ip_allocation_policy {
-    cluster_secondary_range_name  = "pods"
-    services_secondary_range_name = "services"
-  }
-
   node_config {
-    service_account = "gke-node-sa@${var.project_id}.iam.gserviceaccount.com"
+    machine_type    = "e2-medium"
+    service_account = data.google_service_account.gke_workload_sa.email
     oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform"
+      "https://www.googleapis.com/auth/cloud-platform",
     ]
   }
 }
 
-resource "google_container_node_pool" "primary_nodes" {
-  name       = "primary-node-pool"
-  location   = var.region
-  cluster    = google_container_cluster.primary.name
-  node_count = 1
-
-  node_config {
-    machine_type = "e2-medium"       # Free tier-compatible
-    disk_type    = "pd-standard"     # Avoid pd-ssd to prevent quota errors
-    disk_size_gb = 20                # 20 GB is within free tier/quota
-    service_account = "gke-node-sa@${var.project_id}.iam.gserviceaccount.com"
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
-  }
-}
 
