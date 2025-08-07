f445f54d71a821de5830e6f09a802f3a71cccf31
Muralee2 <muralimvkaws1@gmail.com>
2025-08-07 18:23:21 +0530

    Update gke.tf

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -1,21 +1,42 @@
-data "google_service_account" "gke_workload_sa" {
-  account_id = "gke-workload-sa"
-  project    = var.project_id
-}
-
 resource "google_container_cluster" "primary" {
-  name                     = "my-gke-cluster"
-  location                 = var.region
+  name     = var.cluster_name
+  location = var.region
+
+  network    = module.vpc.network_name
+  subnetwork = module.vpc.subnets[0].name
+
   remove_default_node_pool = true
   initial_node_count       = 1
 
-  node_config {
-    machine_type    = "e2-medium"
-    service_account = data.google_service_account.gke_workload_sa.email
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform",
-    ]
+  ip_allocation_policy {
+    cluster_secondary_range_name  = var.pods_range_name
+    services_secondary_range_name = var.services_range_name
+  }
+
+  release_channel {
+    channel = "REGULAR"
+  }
+
+  master_auth {
+    client_certificate_config {
+      issue_client_certificate = false
+    }
   }
 }
 
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "node-pool"
+  cluster    = google_container_cluster.primary.name
+  location   = var.region
+
+  node_config {
+    machine_type = "e2-medium"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+    tags = ["gke-node"]
+  }
+
+  node_count = 2
+}
 
