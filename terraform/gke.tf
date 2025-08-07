c4290925f9835d27fdbf7b4ba2d947ee6adb2b37
Muralee2 <muralimvkaws1@gmail.com>
2025-08-07 21:59:21 +0530

    Update gke.tf

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -40,29 +40,3 @@ resource "google_container_node_pool" "primary_nodes" {
   node_count = 2
 }
 
-# firewall.tf
-resource "google_compute_firewall" "allow_lb_health_checks" {
-  name    = "allow-lb-health-checks"
-  network = module.vpc.network_name
-
-  allow {
-    protocol = "tcp"
-    ports    = ["80", "443"]
-  }
-
-  source_ranges = ["130.211.0.0/22", "35.191.0.0/16"]
-  direction     = "INGRESS"
-  target_tags   = ["gke-node"]
-}
-
-resource "google_compute_firewall" "allow_internal" {
-  name    = "allow-internal"
-  network = module.vpc.network_name
-
-  allow {
-    protocol = "all"
-  }
-
-  source_ranges = ["10.0.0.0/8"]
-  direction     = "INGRESS"
-}
