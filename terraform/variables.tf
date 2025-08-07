d3b356db05cb702009cf44b68cc77e5fae4e856b
Muralee2 <muralimvkaws1@gmail.com>
2025-08-07 18:25:17 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,39 +1,11 @@
-variable "project_id" {
-  type = string
-}
-
-variable "region" {
-  type = string
-}
-
-variable "zone" {
-  description = "GCP Zone"
-  type        = string
-  default     = "us-central1-a" # or your preferred zone
-}
-
-variable "cluster_name" {
-  description = "Name of the GKE cluster"
-  type        = string
-  default     = "secure-gke-cluster" # or remove default if you want to pass it manually
-}
-
-
-variable "firewall_rules" {
-  description = "Map of firewall rules"
-  type = map(object({
-    name               = string
-    protocol           = string
-    ports              = list(string)
-    source_tags        = list(string)
-    target_tags        = list(string)
-    direction          = string
-    source_ranges      = list(string)
-    destination_ranges = list(string)
-  }))
-}
-
-
+variable "project_id" {}
+variable "region" {}
+variable "network_name" {}
+variable "subnet_name" {}
+variable "subnet_ip" {}
+variable "cluster_name" {}
+variable "pods_range_name" {}
+variable "services_range_name" {}
 
 
 
