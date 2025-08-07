7a89bb9caa6c54944cc538e38bfef2ad20dd466e
Muralee2 <muralimvkaws1@gmail.com>
2025-08-07 20:23:49 +0530

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,4 +1,18 @@
-resource "google_compute_network" "vpc" {
-  name                    = "custom-vpc"
-  auto_create_subnetworks = false
+resource "google_compute_subnetwork" "gke_subnet" {
+  name          = var.subnet_name
+  ip_cidr_range = var.subnet_ip
+  region        = var.region
+  network       = google_compute_network.vpc.id
+  purpose       = "PRIVATE"
+  stack_type    = "IPV4_ONLY"
+
+  secondary_ip_range {
+    range_name    = "gke-pods"
+    ip_cidr_range = var.pods_ip_range
+  }
+
+  secondary_ip_range {
+    range_name    = "gke-services"
+    ip_cidr_range = var.services_ip_range
+  }
 }
